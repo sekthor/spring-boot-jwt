a28b07cb46f34ba21958c586cb0606a04923ce24
sekthor <sysdroyed@gmail.com>
2020-02-16 12:30:38 +0100

    added user

--- src/main/java/ch/sekthor/springbootjwt/models/User.java
+++ src/main/java/ch/sekthor/springbootjwt/models/User.java
@@ -1,4 +1,69 @@
 package ch.sekthor.springbootjwt.models;
 
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Collection;
+
+@Entity
 public class User {
+
+    @Id @GeneratedValue
+    private Long userId;
+    private String username;
+    private String password;
+    private String firstname;
+    private String lastname;
+
+    @ElementCollection
+    private Collection<String> authorities;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Collection<String> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Collection<String> authorities) {
+        this.authorities = authorities;
+    }
 }
--- src/main/java/ch/sekthor/springbootjwt/repositories/UserRepository.java
+++ src/main/java/ch/sekthor/springbootjwt/repositories/UserRepository.java
@@ -1,4 +1,14 @@
 package ch.sekthor.springbootjwt.repositories;
 
-public class UserRepository {
+import ch.sekthor.springbootjwt.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(@Param("username") String username);
 }
